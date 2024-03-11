a63e1fa0b55d4373c3266274823818379ac1c5e8
David Wilson <spamdog@gmail.com>
2024-03-11 19:49:32 +1000

    Render triangle in OpenTK

---
+++ Marble.Game/TK/Shader.cs
@@ -0,0 +1,86 @@
+﻿using OpenTK.Graphics.ES30;
+
+namespace Marble.Game.TK
+{
+    public class Shader : IDisposable
+    {
+        public int Handle;
+        public int VertexShader;
+        public int FragmentShader;
+
+        private bool disposedValue = false;
+
+        public Shader()
+        {
+            var VertexShaderSource = File.ReadAllText("Content\\shader.vert");
+            var FragmentShaderSource = File.ReadAllText("Content\\shader.frag");
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+
+            GL.CompileShader(VertexShader);
+
+            GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out int success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(VertexShader);
+                Console.WriteLine(infoLog);
+            }
+
+            GL.CompileShader(FragmentShader);
+
+            GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(FragmentShader);
+                Console.WriteLine(infoLog);
+            }
+
+            Handle = GL.CreateProgram();
+
+            GL.AttachShader(Handle, VertexShader);
+            GL.AttachShader(Handle, FragmentShader);
+
+            GL.LinkProgram(Handle);
+
+            GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out success);
+            if (success == 0)
+            {
+                string infoLog = GL.GetProgramInfoLog(Handle);
+                Console.WriteLine(infoLog);
+            }
+
+            GL.DetachShader(Handle, VertexShader);
+            GL.DetachShader(Handle, FragmentShader);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+
+                disposedValue = true;
+            }
+        }
+
+        ~Shader()
+        {
+            if (disposedValue == false)
+            {
+                Console.WriteLine("GPU Resource leak! Did you forget to call Dispose()?");
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- Marble.Game/TK/Window.cs
+++ Marble.Game/TK/Window.cs
@@ -2,24 +2,25 @@
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Reflection.Metadata;
 
 namespace Marble.Game.TK
 {
     public class Window : GameWindow
     {
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+             0.5f, -0.5f, 0.0f, //Bottom-right vertex
+             0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+        int VertexBufferObject;
+        int VertexArrayObject;
+        Shader shader;
+
         public Window(int width, int height, string title)
             : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
         {
-        }
-
-        protected override void OnUpdateFrame(FrameEventArgs e)
-        {
-            base.OnUpdateFrame(e);
-
-            if (KeyboardState.IsKeyDown(Keys.Escape))
-            {
-                Close();
-            }
         }
 
         protected override void OnLoad()
@@ -27,8 +28,36 @@ namespace Marble.Game.TK
             base.OnLoad();
 
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
 
-            //Code goes here
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            shader = new Shader();
+            GL.UseProgram(shader.Handle);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.UseProgram(shader.Handle);
+
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            SwapBuffers();
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
         }
 
         protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
@@ -36,6 +65,23 @@ namespace Marble.Game.TK
             base.OnFramebufferResize(e);
 
             GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+        protected override void OnUnload()
+        {
+            // Unbind all the resources by binding the targets to 0/null.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            // Delete all the resources.
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteVertexArray(VertexArrayObject);
+
+            //shader.Dispose();
+            GL.DeleteProgram(shader.Handle);
+
+            base.OnUnload();
         }
     }
 }
